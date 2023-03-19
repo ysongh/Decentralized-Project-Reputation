d712507bb2ef9186bf79e9cb99e445a407b96506
You Song Hou <ysongweb3@gmail.com>
2023-03-18 23:26:15 -0400

    feat: update hardhat.config.js to deploy a contract on Scroll Alpha Testnet

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,8 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    // npx hardhat run scripts/deploy.js --network scrollAlpha
+    "scrollAlpha": {
+      url: "https://alpha-rpc.scroll.io/l2",
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 534353
+    },
+  },
   // set the path to compile the contracts
   paths: {
     artifacts: '../client/src/artifacts',
