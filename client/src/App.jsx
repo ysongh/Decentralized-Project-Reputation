9606803c57e9084f7991e6f94fbd5976e13172c7
You Song Hou <ysongweb3@gmail.com>
2023-03-20 01:00:37 -0400

    feat: update the form to add rating for a project to the contract

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <Route
             path="/add-project"
             element={
-              <AddProject />} />
+              <AddProject contractDPR={contractDPR} />} />
           <Route
             path="/"
             element={
--- client/src/pages/AddProject.jsx
+++ client/src/pages/AddProject.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { Container, Center, Box, FormControl, FormLabel, Input, Heading, Button } from '@chakra-ui/react';
 
-function AddProject() {
+function AddProject({ contractDPR }) {
   const [address, setAddress] = useState("");
+  const [num, setNum] = useState("");
 
   const submitProject = async () => {
     try {
-      alert(address);
+      const transaction = await contractDPR.rateAProject(address, num);
+      const tx = await transaction.wait();
+      console.log(tx);
     } catch(error) {
       console.error(error);
     }
@@ -20,6 +23,10 @@ function AddProject() {
           <FormControl mb='3'>
             <FormLabel htmlFor='contractAddress'>Contract Address</FormLabel>
             <Input value={address} onChange={(e) => setAddress(e.target.value)} />
+          </FormControl>
+          <FormControl mb='3'>
+            <FormLabel htmlFor='rate'>Rate</FormLabel>
+            <Input value={num} onChange={(e) => setNum(e.target.value)} />
           </FormControl>
 
           <Button mt="4" onClick={submitProject}>Add</Button>
