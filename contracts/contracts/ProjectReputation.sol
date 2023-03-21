003dbdd3bd171c42fdf16333b7d1a7b27f63ce03
You Song Hou <ysongweb3@gmail.com>
2023-03-21 19:56:54 -0400

    feat: update the contract to include Project struct

--- contracts/contracts/ProjectReputation.sol
+++ contracts/contracts/ProjectReputation.sol
@@ -2,18 +2,37 @@
 pragma solidity ^0.8.18;
 
 contract ProjectReputation {
-    mapping(address => uint[]) public ratings;
+    uint public projectCount = 0;
 
-    event rated(address to, uint num, address from);
+    uint numberOfChallenges;
+    Project[] public projects;
+
+    struct Project{
+        uint id;
+        string name;
+        uint[] ratings;
+        address[] voters;
+        address owner;
+    }
+
+    event rated(uint id, uint num, address from);
 
     constructor() {}
 
-    function rateAProject(address to, uint num) external {
-        ratings[to].push(num);
-        emit rated(to, num, msg.sender);
+    function addProject(string memory name) external {
+        projects.push(Project(projectCount, name, new uint[](0), new address[](0), msg.sender));
+        projectCount++;
     }
 
-    function getProjectRating(address to) external view returns (uint[] memory) {
-        return ratings[to];
+    function rateAProject(uint id, uint num) external {
+        require(num < 6, "Rate must be less than 5");
+        
+        projects[id].ratings.push(num);
+        projects[id].voters.push(msg.sender);
+        emit rated(id, num, msg.sender);
+    }
+
+    function getProjects() external view returns (Project[] memory) {
+        return projects;
     }
 }
